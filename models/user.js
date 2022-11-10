80dee87dd7ed196246c38e1f9c81472c9b946161
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-11-10 15:48:24 -0500

    Added users tableName

--- models/user.js
+++ models/user.js
@@ -1,31 +1,33 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    email: DataTypes.STRING,
-    phoneNumber: DataTypes.STRING,
-    age: DataTypes.INTEGER,
-    profilePicture: DataTypes.STRING,
-    about: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
+	class User extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	User.init(
+		{
+			firstName: DataTypes.STRING,
+			lastName: DataTypes.STRING,
+			username: DataTypes.STRING,
+			password: DataTypes.STRING,
+			email: DataTypes.STRING,
+			phoneNumber: DataTypes.STRING,
+			age: DataTypes.INTEGER,
+			profilePicture: DataTypes.STRING,
+			about: DataTypes.STRING
+		},
+		{
+			sequelize,
+			modelName: 'User',
+			tableName: 'users'
+		}
+	);
+	return User;
 };
