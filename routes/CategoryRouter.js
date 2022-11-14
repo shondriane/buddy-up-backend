8982aea95c09fcc5e3fb3800763f77fef618cd1b
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-11-14 11:20:44 -0500

    Added authentication to additional routes

--- routes/CategoryRouter.js
+++ routes/CategoryRouter.js
@@ -3,8 +3,23 @@ const controller = require('../controllers/CategoryController');
 
 Router.get('/', controller.GetAllCategories);
 Router.get('/:category_id', controller.GetCategoryById);
-Router.post('/', controller.CreateCategory);
-Router.put('/:category_id', controller.UpdateCategoryById);
-Router.delete('/:category_id', controller.DeleteCategoryById);
+Router.post(
+	'/',
+	middleware.stripToken,
+	middleware.verifyToken,
+	controller.CreateCategory
+);
+Router.put(
+	'/:category_id',
+	middleware.stripToken,
+	middleware.verifyToken,
+	controller.UpdateCategoryById
+);
+Router.delete(
+	'/:category_id',
+	middleware.stripToken,
+	middleware.verifyToken,
+	controller.DeleteCategoryById
+);
 
 module.exports = Router;
