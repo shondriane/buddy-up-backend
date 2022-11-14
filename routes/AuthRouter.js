2275f13aadba45309b7f17fbe97c2652d6ba2b60
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-11-14 10:42:53 -0500

    Built login function

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,24 +1,14 @@
-const Router = require('express').Router()
-const controller = require('../controllers/AuthController')
-const middleware = require ('../middleware')
+const Router = require('express').Router();
+const controller = require('../controllers/AuthController');
+const middleware = require('../middleware');
 
-Router.post('/register',controller.Register)
-Router.post('/login',controller.Login)
-Router.put('/update',
- middleware.stripToken,
- middleware.verifyToken,
-//  controller.UpdatePassword
- )
-
-
-
-
-
-
-
-
-
-
-
+Router.post('/register', controller.Register);
+Router.post('/login', controller.Login);
+Router.put(
+	'/update',
+	middleware.stripToken,
+	middleware.verifyToken
+	//  controller.UpdatePassword
+);
 
 module.exports = Router;
