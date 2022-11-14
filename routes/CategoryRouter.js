c89e3ab68de5537416bfde42f3de15f3915177a8
Aekangi Patel <aekangi91@gmail.com>
2022-11-14 11:58:49 -0500

    heroku

--- routes/CategoryRouter.js
+++ routes/CategoryRouter.js
@@ -1,25 +1,26 @@
-const Router = require('express').Router();
-const controller = require('../controllers/CategoryController');
+const Router = require('express').Router()
+const controller = require('../controllers/CategoryController')
+const middleware = require('../middleware')
 
-Router.get('/', controller.GetAllCategories);
-Router.get('/:category_id', controller.GetCategoryById);
+Router.get('/', controller.GetAllCategories)
+Router.get('/:category_id', controller.GetCategoryById)
 Router.post(
-	'/',
-	middleware.stripToken,
-	middleware.verifyToken,
-	controller.CreateCategory
-);
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreateCategory
+)
 Router.put(
-	'/:category_id',
-	middleware.stripToken,
-	middleware.verifyToken,
-	controller.UpdateCategoryById
-);
+  '/:category_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdateCategoryById
+)
 Router.delete(
-	'/:category_id',
-	middleware.stripToken,
-	middleware.verifyToken,
-	controller.DeleteCategoryById
-);
+  '/:category_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeleteCategoryById
+)
 
-module.exports = Router;
+module.exports = Router
