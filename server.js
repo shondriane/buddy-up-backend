f21dfec9a08358d2b7eb4c78f38a2054410fd869
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-11-15 14:32:02 -0500

    Removed logger

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const logger = require('morgan')
 const app = express();
 
 const AppRouter = require('./routes/AppRouter');
@@ -8,7 +7,6 @@ const AppRouter = require('./routes/AppRouter');
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(logger('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
