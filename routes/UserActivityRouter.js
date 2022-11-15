ab81979e421a1b93a85ab5a5ae122f1f1bc4306c
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-11-15 15:50:10 -0500

    Updated UserActivity router

--- routes/UserActivityRouter.js
+++ routes/UserActivityRouter.js
@@ -2,11 +2,20 @@ const Router = require('express').Router();
 const controller = require('../controllers/UserActivityController');
 
 Router.get('/', controller.GetAllUserActivities);
-Router.get('/all', controller. GetAllUsersAndActivities);
+Router.get('/all', controller.GetAllUsersAndActivities);
 Router.get('/:user_activity_id', controller.GetAllUsersAndActivitiesById);
-Router.get('/user/:user_created_id', controller.GetAllUsersAndActivitiesByUserId);
-Router.get('/activity/:activity_listed_id', controller.GetAllUsersAndActivitiesByActivityId);
-Router.post('/', controller.CreateUserActivity);
+Router.get(
+	'/user/:user_created_id',
+	controller.GetAllUsersAndActivitiesByUserId
+);
+Router.get(
+	'/activity/:activity_listed_id',
+	controller.GetAllUsersAndActivitiesByActivityId
+);
+Router.post(
+	'/user/:user_created_id/activity/:activity_id',
+	controller.CreateUserActivity
+);
 Router.put('/:user_activity_id', controller.UpdateUserActivityById);
 Router.delete('/:user_activity_id', controller.DeleteUserActivityById);
 
