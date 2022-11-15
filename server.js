b4ba4555741ea95ae1e9bd13a362865a51bc10e4
shondriane <shondriane.wise@yahoo.com>
2022-11-15 13:58:18 -0500

    fixing backend rout for auth

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const logger = require('morgan')
 const app = express();
 
 const AppRouter = require('./routes/AppRouter');
@@ -8,6 +8,7 @@ const AppRouter = require('./routes/AppRouter');
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
+app.use(logger('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
