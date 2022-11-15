46b8cae396deafd8b0c41bd180e08668c5d99861
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-11-15 09:31:15 -0500

    Test deployment

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
-require('dotenv').config()
+require('dotenv').config();
 module.exports = {
-  development: {
-    database: 'buddy_up_development',
-    dialect: 'postgres'
-  },
-  test: {
-    database: 'buddy_up_test',
-    dialect: 'postgres'
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false,
-        require: true
-      }
-    }
-  }
-}
+	development: {
+		database: 'buddy_up_development',
+		dialect: 'postgres'
+	},
+	test: {
+		database: 'buddy_up_test',
+		dialect: 'postgres'
+	},
+	production: {
+		use_env_variable: 'DATABASE_URL',
+		dialect: 'postgres',
+		dialectOptions: {
+			ssl: {
+				rejectUnauthorized: false,
+				require: true
+			}
+		}
+	}
+};
