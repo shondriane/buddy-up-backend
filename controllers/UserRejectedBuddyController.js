5880a4bf9f9fe6ed528d29500afed571ae0ca39b
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-11-16 22:20:50 -0500

    Updated post controller for UserRejectedBuddy with raw query

--- controllers/UserRejectedBuddyController.js
+++ controllers/UserRejectedBuddyController.js
@@ -1,4 +1,5 @@
-const { User, UserRejectedBuddy } = require('../models');
+const { User, UserRejectedBuddy, sequelize } = require('../models');
+const db = require('../models/index');
 
 const GetAllUserNotBuddyFollowers = async (req, res) => {
 	try {
@@ -88,11 +89,15 @@ const CreateUserRejectedBuddy = async (req, res) => {
 	try {
 		const userId = parseInt(req.params.user_id);
 		const rejectedBuddyId = parseInt(req.params.rejected_buddy_id);
-		const userRejectedBuddy = await UserRejectedBuddy.create({
-			userId,
-			rejectedBuddyId,
-			...req.body
-		});
+		// const userRejectedBuddy = await UserRejectedBuddy.create({
+		// 	userId,
+		// 	rejectedBuddyId,
+		// 	...req.body
+		// });
+		const userRejectedBuddy = await db.sequelize.query(
+			`INSERT INTO "user_rejected_buddies" ("userId","rejectedBuddyId","createdAt","updatedAt") VALUES (${userId},${rejectedBuddyId},current_timestamp,current_timestamp) RETURNING "userId","rejectedBuddyId","createdAt","updatedAt","id";`,
+			{ type: sequelize.QueryTypes.INSERT }
+		);
 		res.send(userRejectedBuddy);
 	} catch (error) {
 		throw error;
